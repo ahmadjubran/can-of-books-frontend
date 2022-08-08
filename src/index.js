b7d3678f417d5dae828197a3fa5d89fecb85240e
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-08-08 21:21:40 +0300

    Add authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-zmbe0el4.us.auth0.com"
+      clientId="pGldpS7nQ2nsHIkbpjDQOolP41wDqmbe"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
